174b40a51927942f4279cbcc78d8d6d756a50197
Paulo Lima <paulobruny@gmail.com>
2020-08-04 15:29:40 -0300

    finish grid layout and desktop styling

--- src/pages/Landing/styles.ts
+++ src/pages/Landing/styles.ts
@@ -13,24 +13,6 @@ export const pageLanding = styled.div`
 
 `
 
-export const pageLandingContent = styled.div`
-  @media (min-width: 1100px) {
-    max-width: 1100px;
-    display: grid;
-    grid-template-rows: 350px 1fr;
-    grid-template-columns: 2fr 1fr 1fr;
-    grid-template-areas: 
-      "logo hero hero"
-      "buttons buttons total";
-  }
-
-  .hero-image {
-    width: 100%;
-    grid-area: hero;
-  }
-
-`
-
 export const logoContainer = styled.div`
   text-align: center;
   margin-bottom: 3.2rem;
@@ -101,4 +83,55 @@ export const totalConnections = styled.span`
   img {
     margin-left: 0.8rem;
   }
+`
+
+
+export const pageLandingContent = styled.div`
+  @media (min-width: 1100px) {
+    max-width: 1100px;
+    display: grid;
+    grid-template-rows: 350px 1fr;
+    grid-template-columns: 2fr 1fr 1fr;
+    grid-template-areas: 
+      "logo hero hero"
+      "buttons buttons total";
+  }
+
+  .hero-image {
+    width: 100%;
+    grid-area: hero;
+    justify-self: end;
+  }
+
+  ${logoContainer} {
+    align-self: center;
+    margin: 0;
+    text-align: left;
+
+    h2 {
+      text-align: initial;
+      font-size: 3.6rem;
+    }
+
+    img {
+      height: 100%;
+    }
+  }
+
+  ${buttonsContainer} {
+    justify-content: flex-start;
+
+    a {
+      font-size: 2.4rem;
+    }
+
+    a img {
+      margin-right: 2.4rem;
+    }
+  }
+
+  ${totalConnections} {
+    justify-self: end;
+  }
+
 `
