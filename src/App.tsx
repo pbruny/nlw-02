27faac0f917e43206f8835c85a88a01d71887087
Paulo Lima <paulobruny@gmail.com>
2020-08-03 21:56:55 -0300

    Add landing component to App and styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import React from "react"
+import React from 'react'
+import Landing from './pages/Landing'
+import './assets/styles/global.css'
 
 function App() {
   return (
     <>
-      <h1>Hallo World!</h1>
+      <Landing />
     </>
   )
 }
